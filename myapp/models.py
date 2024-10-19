7c66750ef74903da38b9f1a2ab1f256ca12457c3
yishiv2 <yishiv2@users.noreply.github.com>
2024-10-19 20:57:43 +0900

    文字数制限を追加

--- myapp/models.py
+++ myapp/models.py
@@ -15,7 +15,7 @@ Base = declarative_base()
 class Item(Base):
     __tablename__ = "items"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+    name = Column(String(255), index=True)
 
 
 async def get_db() -> AsyncSession:
